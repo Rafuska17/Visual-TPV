8e4eae54be01d2d7698bb1c82f33535b6176ed71
Rafuska17 <Rafuska_17@hotmail.com>
2016-11-20 18:59:04 +0100

    Tpv4.0

---
+++ LibreriaValidaciones/LibreriaValidaciones/Class1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Validaciones
+    Public Function validar(ByVal text As String) As Boolean
+        Dim cha As Char
+        Dim nv As String = "!¡?¿+-$%&/()=""'@*\|ºª€{}[];:,.·¬<>"
+        Dim ch2 As Char
+
+        For i As Integer = 1 To text.Length
+            cha = GetChar(text, i)
+            If IsNumeric(cha) Then
+                MsgBox("Tiene que ser una letra")
+                Return False
+            Else
+                For k As Integer = 1 To nv.Length
+                    ch2 = GetChar(nv, k)
+                    If cha.Equals(ch2) Then
+                        MsgBox("Tiene que ser un caracter valido")
+                        Return False
+                    End If
+                Next
+            End If
+        Next
+        Return True
+    End Function
+
+    Public Function validartelefono(ByVal text As String) As Boolean
+
+        Dim cha As Char
+        For i As Integer = 1 To text.Length
+            cha = GetChar(text, i)
+            If IsNumeric(cha) Or cha.Equals("+") Then
+                Return True
+            Else
+                MsgBox("Tiene que ser una letra")
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+    Public Function Nif(ByVal num As Long) As String
+        Dim tab() As String = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "O", "H", "L", "C", "K", "E"}
+        Return tab(num Mod 23)
+    End Function
+End Class
--- ProyectoTPVfinal/GestionEmpleados.vb
+++ ProyectoTPVfinal/GestionEmpleados.vb
@@ -1,13 +1,12 @@
 ﻿Imports LibreriaValidaciones
+Imports System.IO
 
 Public Class GestionEmpleados
     Private Sub GestionEmpleados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         limpiar()
-
-
     End Sub
 
-    Private Sub limpiar()
+Private Sub limpiar()
         TbCodEmple.Clear()
         TbNombre.Clear()
         TbApellido1.Clear()
@@ -16,23 +15,32 @@ Public Class GestionEmpleados
         TbTelefono.Clear()
     End Sub
 
-    Private Sub RbAlta_CheckedChanged(sender As Object, e As EventArgs) Handles RbAlta.CheckedChanged
-        Dim vali As New Validaciones
-        FileOpen(1, "Empleados.txt", OpenMode.Random, OpenAccess.Write, , Len(user))
-        Dim a As Integer = 1
-        user.nomuser = vali.validar(TbNombreUser.Text)
-        user.contrauser = vali.validar(TbContraUser.Text)
-        user.nom = vali.validar(TbNombre.Text)
-        user.ape1 = vali.validar(TbApellido1.Text)
-        user.ape2 = vali.validar(TbApellido1.Text)
-        user.dni = vali.Nif(TbDni.Text)
-        user.tel = vali.
+    Private Sub BtAceptar_Click(sender As Object, e As EventArgs) Handles BtAceptar.Click
+        If RbAlta.Checked Then
+            Dim vali As New Validaciones
+            FileOpen(1, "Empleados.txt", OpenMode.Random, OpenAccess.Write, , Len(user))
+            Dim aux As String = TbDni.Text.TrimEnd
+            Dim aux2 As String = aux + vali.Nif(aux).ToString
 
-        FilePut(1, user, a)
-        FileClose()
-    End Sub
+            If TbDni.Text.Equals(aux2) Then
+                Dim a As Integer = Val(TbCodEmple.Text)
+                user.nomuser = vali.validar(TbNombreUser.Text)
+                user.contrauser = vali.validar(TbContraUser.Text)
+                user.cod = a
+                user.nom = vali.validar(TbNombre.Text)
+                user.ape1 = vali.validar(TbApellido1.Text)
+                user.ape2 = vali.validar(TbApellido1.Text)
+                user.dni = vali.Nif(TbDni.Text)
+                user.tel = vali.validartelefono(TbTelefono.Text)
 
-    Private Sub RbModificar_CheckedChanged(sender As Object, e As EventArgs) Handles RbModificar.CheckedChanged
+                FilePut(1, user, a)
+                FileClose()
+            Else
+                MsgBox("Nombre de Usuario o Contraseña incorrecta, pruebe de nuevo", 4144, "Error de inicio de sesion")
+            End If
+        ElseIf RbModificar.Checked Then
+        ElseIf RbBorrar.Checked Then
 
+        End If
     End Sub
 End Class
